d3fccd1d07d061efe0792f559e5781c5afef15ce
ravi-git-22 <ravikiran.thames@outlook.com>
2023-02-14 23:21:06 +0000

    commit-6

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "azure_registry_resource_group" {
 }
 
 resource "azurerm_container_registry" "terraform_docker_acr" {
-  name                = "terraformdockeracrravi"
+  name                = "dockeracrravi"
   resource_group_name = azurerm_resource_group.azure_registry_resource_group.name
   location            = azurerm_resource_group.azure_registry_resource_group.location
   sku                 = "Basic"
